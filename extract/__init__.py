3192dee315f72057fbaa95997e4fbe0cc15e7639
Matthew Gardner <mj.gardner@hotmail.com>
2023-02-24 18:52:01 +0000

    Added Ticket 4 functionality

--- extract/__init__.py
+++ extract/__init__.py
@@ -22,17 +22,25 @@ import csv
 #               input data file.
 #Objectives:    A final array is created with any empty lines omitted.
 
+#Ticket #4:     Capitalise user name fields
+#Description:   Add functionality to your input script to automatically capitalise the first_name
+#               and last_name fields found in the input data.
+#Objectives:    All names are capitalised in all data entries
+
 def get_input(filename):
 
     #Initialise list that values will be appneded to
-    expected_output=[]
+    cleansed_list=[]
+    final_list=['user_id','first_name','last_name','answer_1','answer_2','answer_3']
 
     with open(filename, 'r') as f: #Open the input file
         
         #Read the CSV file into a list of dictionaries
+        #Ticket #1
         reader = csv.DictReader(f)
         
         #Create a set to store unique values
+        #Ticket #2
         unique_values = set()
 
         #Iterate over each row in the input file
@@ -42,18 +50,34 @@ def get_input(filename):
             
             #If the value has not already been seen, add it to the set and write the row to the output file
             if field_value not in unique_values:
-                #If statement to cover off Ticket 3.  Will ignore empty lines
+                #Ticket #3: If statement to cover off Ticket 3.  Will ignore empty lines
                 if len(field_value) > 0:
                     unique_values.add(field_value)
-                    #print(row)
-                    expected_output.append(row)
-    
-    #Print final list of values.
-    print(expected_output)
+                    cleansed_list.append(row)
+        
+        #Ticket #4: capitilize name field
+        #changing from list of dictionaries to a final list
+        i = 0
+        for rows in cleansed_list:
+            final_list.append(cleansed_list[i]['user_id'])
+            final_list.append(cleansed_list[i]['first_name'].capitalize())
+            final_list.append(cleansed_list[i]['last_name'].capitalize())
+            final_list.append(cleansed_list[i]['answer_1'])
+            final_list.append(cleansed_list[i]['answer_2'])
+            final_list.append(cleansed_list[i]['answer_3'])
+            i += 1
 
+    return list(final_list)
 
 def file_exists(filename):
     try:
         open(filename)
     except FileNotFoundError:
         print("File not found")
+    return
+
+def capitalize_name():
+    for capitilized_list in cleansed_list:
+        #capitilized_list[0] = capitilized_list[0]
+        print(cleansed_list[2])
+    return
--- survey.py
+++ survey.py
@@ -1,19 +1,6 @@
 import sys
 from extract import get_input, file_exists
 
-#Ticket #1:   Read a CSV file
-#Description: In your input script, create a function that will read data from a CSV file.
-#Objectives:  The results.csv data file can be successfully processed into an array.
-#             Each line of the file is read into a new array item.
-#             The file read method must be in a sub-module.
-
-#Ticket #2:     Remove duplicate entries
-#Description:   Add functionality to your input script to ignore or remove any duplicate entries
-#from the input data.
-#Duplicates are based on the User Id field.
-#Objectives:    A final array is created with duplicate entries removed.
-#               Where duplicates are found, the first entry is retained.
-
 def get_args():
     try:
         return(sys.argv[1])
@@ -30,6 +17,8 @@ def main():
     filename = get_args() #Get filename for sys arg
     file_exists(filename) # Check if file exists
     mylist = get_input(filename)
+    print(mylist)
+    #capitalize_name(mylist)
 
 
 
